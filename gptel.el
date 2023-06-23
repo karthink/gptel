18fdf04e6771d7118fbcde5bafd43cb2a8273e8a
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2023-06-23 17:10:12 +0900

    fix: add longer context variants + rm soon to be deprecated snapshot models

--- gptel.el
+++ gptel.el
@@ -189,10 +189,9 @@ will get progressively longer!"
 
 The current options are
 - \"gpt-3.5-turbo\"
-- \"gpt-3.5-turbo-0301\"
-- \"gpt-3.5-turbo-0613\"
 - \"gpt-3.5-turbo-16k\"
 - \"gpt-4\" (experimental)
+- \"gpt-4-32k\" (experimental)
 
 To set the model for a chat session interactively call
 `gptel-send' with a prefix argument."
@@ -200,10 +199,9 @@ To set the model for a chat session interactively call
   :group 'gptel
   :type '(choice
           (const :tag "GPT 3.5 turbo" "gpt-3.5-turbo")
-          (const :tag "GPT 3.5 turbo 0301" "gpt-3.5-turbo-0301")
-          (const :tag "GPT 3.5 turbo 0613" "gpt-3.5-turbo-0613")
           (const :tag "GPT 3.5 turbo 16k" "gpt-3.5-turbo-16k")
-          (const :tag "GPT 4 (experimental)" "gpt-4")))
+          (const :tag "GPT 4 (experimental)" "gpt-4")
+          (const :tag "GPT 4 32k (experimental)" "gpt-4-32k")))
 
 (defcustom gptel-temperature 1.0
   "\"Temperature\" of ChatGPT response.
