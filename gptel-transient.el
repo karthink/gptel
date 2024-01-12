7077319b90490b66e79155926672bfd29a1d9593
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2024-01-11 17:41:09 -0800

    gptel-transient: Show descriptions of directives
    * gptel-transient.el (gptel-system-prompt--setup): (Tentative
    change) Show descriptions of directives next to the keys when
    picking a directive from the system-prompt menu.

--- gptel-transient.el
+++ gptel-transient.el
@@ -177,11 +177,21 @@ which see."
        ;; are treated as suffixes when invoking `gptel-system-prompt' directly,
        ;; and infixes when going through `gptel-menu'.
        ;; TODO: Raise an issue with Transient.
-       collect (list (key-description key) (capitalize name)
-                `(lambda () (interactive)
-                  (message "Directive: %s" ,prompt)
-                  (setq gptel--system-message ,prompt))
-		:transient 'transient--do-return)
+       collect (list (key-description key)
+                     (concat (capitalize name) " "
+                             (propertize " " 'display '(space :align-to 20))
+                             (propertize
+                              (concat
+                               "("
+                               (string-replace
+                                "\n" " "
+                                (truncate-string-to-width prompt (- (window-width) 30) nil nil t))
+                               ")")
+                              'face 'shadow))
+                     `(lambda () (interactive)
+                        (message "Directive: %s" ,prompt)
+                        (setq gptel--system-message ,prompt))
+		     :transient 'transient--do-return)
        into prompt-suffixes
        finally return
        (nconc
