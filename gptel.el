26494197836dfdce9b7910a39c1f1f6f573a68df
Norio Suzuki <norio.suzuki@gmail.com>
2024-04-17 09:32:21 +0900

    gptel: Modify a comment for following current choices

--- gptel.el
+++ gptel.el
@@ -399,9 +399,12 @@ by the LLM provider's API.
 The current options for ChatGPT are
 - \"gpt-3.5-turbo\"
 - \"gpt-3.5-turbo-16k\"
-- \"gpt-4\" (experimental)
-- \"gpt-4-1106-preview\" (experimental)
- 
+- \"gpt-4\"
+- \"gpt-4-turbo\"
+- \"gpt-4-turbo-preview\"
+- \"gpt-4-32k\"
+- \"gpt-4-1106-preview\"
+
 To set the model for a chat session interactively call
 `gptel-send' with a prefix argument."
   :safe #'always
