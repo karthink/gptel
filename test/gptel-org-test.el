3116a230167215e48e666a35f48cf69056fb4a9b
Maciej Barć <xgqt@xgqt.org>
2024-10-03 00:14:17 +0200

    [tests] Refactor tests (explicit require, no byte-compile) (#398)
    * fix(test/gptel-org-test): explicitly require needed gptel-org
    
    * chore(test/gptel-org-test): turn off byte-compiling for tests
    
    ---------
    
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- test/gptel-org-test.el
+++ test/gptel-org-test.el
@@ -1,6 +1,7 @@
-;; -*- lexical-binding: t; -*-
+;; -*- lexical-binding: t; no-byte-compile: t; -*-
 (require 'ert)
 (require 'gptel)
+(require 'gptel-org)
 (require 'cl-generic)
 
 (declare-function json-read "json" ())
