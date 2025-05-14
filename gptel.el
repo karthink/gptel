fd9200b55979c9b262b269a2e4b5eb62ffc4253c
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2025-05-13 23:44:38 -0700

    gptel: Reset reasoning-block after tool call
    * gptel.el (gptel--handle-wait): Reset the :reasoning-block key, which
    is used to track the handling of reasoning content, before sending
    back the tool call results.  This is so the reasoning content in
    the tool result analysis part of the response is correctly captured.

--- gptel.el
+++ gptel.el
@@ -1998,7 +1998,8 @@ buffer."
   ;; a second network request: gptel tests for the presence of these flags to
   ;; handle state transitions.  (NOTE: Don't add :token to this.)
   (let ((info (gptel-fsm-info fsm)))
-    (dolist (key '(:tool-success :tool-use :error :http-status :reasoning))
+    (dolist (key '(:tool-success :tool-use :error
+                   :http-status :reasoning :reasoning-block))
       (when (plist-get info key)
         (plist-put info key nil))))
   (funcall
