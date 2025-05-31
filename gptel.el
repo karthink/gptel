104fa5d7bc7f561d7ae321706943117b46e4f660
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2025-05-31 11:50:53 -0700

    gptel: Update NEWS, README, package commentary
    NEWS: Fill out in prep for new release.
    
    README: Mention `gptel-prompt-transform-functions', add more
    mentions of presets.
    
    gptel.el: Update package commentary with new backends and
    description of the presets feature.

--- gptel.el
+++ gptel.el
@@ -34,9 +34,10 @@
 ;;
 ;; gptel supports:
 ;;
-;; - The services ChatGPT, Azure, Gemini, Anthropic AI, Together.ai,
-;;   Perplexity, Anyscale, OpenRouter, Groq, PrivateGPT, DeepSeek, Cerebras,
-;;   Github Models, Novita AI, xAI and Kagi (FastGPT & Summarizer).
+;; - The services ChatGPT, Azure, Gemini, Anthropic AI, Together.ai, Perplexity,
+;;   Anyscale, OpenRouter, Groq, PrivateGPT, DeepSeek, Cerebras, Github Models,
+;;   GitHub Copilot chat, AWS Bedrock, Novita AI, xAI, Sambanova, Mistral Le
+;;   Chat and Kagi (FastGPT & Summarizer).
 ;; - Local models via Ollama, Llama.cpp, Llamafiles or GPT4All
 ;;
 ;; Additionally, any LLM service (local or remote) that provides an
@@ -158,6 +159,15 @@
 ;; In any buffer: with a region selected, you can rewrite prose, refactor code
 ;; or fill in the region.  This is accessible via `gptel-rewrite', and also from
 ;; the `gptel-send' menu.
+;;
+;; Presets
+;;
+;; Define a bundle of configuration (model, backend, system message, tools etc)
+;; as a "preset" that can be applied together, making it easy to switch between
+;; tasks in gptel.  Presets can be saved and applied from gptel's transient
+;; menu.  You can also include a cookie of the form "@preset-name" in the prompt
+;; to send a request with a preset applied.  This feature works everywhere, but
+;; preset cookies are also fontified in chat buffers.
 ;;
 ;; gptel in Org mode:
 ;;
