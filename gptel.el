a5af15c770b66a61d0609a736e1db37495655559
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2025-06-11 01:57:39 -0700

    gptel: Bump version to 0.9.8.5, add release date
    * gptel.el: Bump version
    * NEWS (0.9.8.5): Add date

--- gptel.el
+++ gptel.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023-2025  Karthik Chikmagalur
 
 ;; Author: Karthik Chikmagalur <karthik.chikmagalur@gmail.com>
-;; Version: 0.9.8
+;; Version: 0.9.8.5
 ;; Package-Requires: ((emacs "27.1") (transient "0.7.4") (compat "29.1.4.1"))
 ;; Keywords: convenience, tools
 ;; URL: https://github.com/karthink/gptel
