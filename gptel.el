1c6fc68e09c383613a68daf6b24878890a03757f
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2025-07-12 16:22:25 +0900

    Don't pin compat. transient has already pinned compat

--- gptel.el
+++ gptel.el
@@ -4,7 +4,7 @@
 
 ;; Author: Karthik Chikmagalur <karthik.chikmagalur@gmail.com>
 ;; Version: 0.9.8.5
-;; Package-Requires: ((emacs "27.1") (transient "0.7.4") (compat "29.1.4.1"))
+;; Package-Requires: ((emacs "27.1") (transient "0.7.4"))
 ;; Keywords: convenience, tools
 ;; URL: https://github.com/karthink/gptel
 
