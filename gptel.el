0d8d8e9f16bb434ac6186a97327e9af3d67c2823
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2026-01-05 15:59:19 -0800

    gptel: Fontify preset indicators correctly
    * gptel.el (gptel--fontify-preset-keyword): When fontifying, match
    presets with not-space, not not-blank, since we don't want to
    include newlines.  See previous commit for details.

--- gptel.el
+++ gptel.el
@@ -2277,7 +2277,7 @@ point is placed at its position."
   "Font-lock function for preset indicators in chat buffers.
 
 Return preset fontification info for text up to END."
-  (and (re-search-forward "@\\([^[:blank:]]+\\)\\_>" end t)
+  (and (re-search-forward "@\\([^[:space:]]+\\)\\_>" end t)
        (or (= (match-beginning 0) (point-min))
            (memq (char-syntax (char-before (match-beginning 0))) '(32 62)))
        (not (plist-get (text-properties-at (match-beginning 1)) 'gptel))))
