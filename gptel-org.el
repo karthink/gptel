33576c6ded4f11d036854c30395cb6ef1aba9f6f
Petri Hiipakka <petri@siroic.com>
2026-01-13 13:58:11 +0200

    debug: Add more tracing to gptel-org--adjust-response-headings

--- gptel-org.el
+++ gptel-org.el
@@ -1102,9 +1102,12 @@ headings in the response should be at level 5 or deeper."
              (save-excursion
                (goto-char beg)
                (if (re-search-backward org-outline-regexp-bol nil t)
-                   (org-outline-level)
+                   (prog1 (org-outline-level)
+                     (message "DEBUG adjust-headings: found heading=%S level=%s"
+                              (org-get-heading t t t t) (org-outline-level)))
                  1)))
             (min-response-level nil))
+        (message "DEBUG adjust-headings: beg=%s end=%s assistant-level=%s" beg end assistant-level)
         (save-restriction
           (narrow-to-region beg end)
           ;; First pass: find the minimum heading level in the response
@@ -1114,16 +1117,19 @@ headings in the response should be at level 5 or deeper."
               (when (or (null min-response-level)
                         (< level min-response-level))
                 (setq min-response-level level))))
+          (message "DEBUG adjust-headings: min-response-level=%s" min-response-level)
           ;; Second pass: adjust headings if needed
           (when (and min-response-level
                      (<= min-response-level assistant-level))
             ;; Need to demote all headings by (assistant-level - min-response-level + 1)
             (let ((level-diff (- (1+ assistant-level) min-response-level)))
+              (message "DEBUG adjust-headings: level-diff=%s, will adjust" level-diff)
               (goto-char (point-min))
               (while (re-search-forward "^\\(\\*+\\)\\( \\)" nil t)
                 (let* ((current-stars (match-string 1))
                        (new-level (+ (length current-stars) level-diff))
                        (new-stars (make-string new-level ?*)))
+                  (message "DEBUG adjust-headings: replacing %s with %s" current-stars new-stars)
                   (replace-match (concat new-stars "\\2")))))))))))
 
 (add-hook 'gptel-post-response-functions #'gptel-org--adjust-response-headings)
