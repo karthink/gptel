8914dcd2b9414e559fed7941e843c4ace742bd0a
Tim Ruffing <dev@real-or-random.org>
2026-02-06 08:11:07 +0100

    Bump minimum transient version to 0.7.8
    * gptel.el: Bump minimum transient version from 0.7.4 (2024-08-05) to
    0.7.8 (2024-11-02).  This will be required for :environment as used in
    then next commit.
    * README.org: Same.
    * NEWS: Mention this change.

--- gptel.el
+++ gptel.el
@@ -4,7 +4,7 @@
 
 ;; Author: Karthik Chikmagalur <karthik.chikmagalur@gmail.com>
 ;; Version: 0.9.9.3
-;; Package-Requires: ((emacs "27.1") (transient "0.7.4") (compat "30.1.0.0"))
+;; Package-Requires: ((emacs "27.1") (transient "0.7.8") (compat "30.1.0.0"))
 ;; Keywords: convenience, tools
 ;; URL: https://github.com/karthink/gptel
 
