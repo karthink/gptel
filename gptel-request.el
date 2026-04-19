karthink/gptel

commit 85126d398c800dfed80a91aac990a5dc0d15f9a6

gptel-request: Improve security of authentication with Curl
Rather than supplying authentication keys on the curl command line
or embedding them in the URL, where they may be visible to other
users on a multi-user system, pass them (more) securely through
stdin via a pipe.

This commit does not affect gptel-bedrock.el which still uses
the old method.

* gptel-request.el (gptel-curl--get-args): Use -H@- to make curl
read the headers from stdin instead of passing them on the command
line.

Introduce a new argument `include-headers' to include headers in
the command line.  This is currently used only by the dry-run
inspector to copy portable Curl commands.

* gptel-request.el (gptel-curl-get-response): Headers are computed
either in `gptel-curl--get-args' or in `gptel-curl-get-response',
but never in both places.  It is important to compute headers only
once per request because they can have side-effects.  This is a
feature, see #1224 for an upcoming use case.

Let-bind `gptel-backend' and `gptel-model' around calls to header
functions, as we do in `gptel-curl--get-args'.  This is required
because the backend header function relies on its environment
to (for example) decrypt API keys, which in turn dispatches on
`gptel-backend'.  Several other potential edge cases are avoided
this way.

Move header logging to `gptel-curl-get-response'.

* gptel.el (gptel--continue-query): Call `gptel-curl--get-arguments'
with `headers' as t if copying the command to kill-ring, or nil
with normal operation.

* NEWS: Mention change.

gptel-request.el
@@ -2642,10 +2642,12 @@ See `gptel-curl--get-response' for its contents.")
 
 ;;; Curl request response handling
 
-(defun gptel-curl--get-args (info uuid)
+(defun gptel-curl--get-args (info uuid include-headers)
   "Produce list of arguments for calling Curl.
 
-INFO contains the request data, UUID is a unique identifier."
+INFO contains the request data, UUID is a unique identifier.
+
+If INCLUDE-HEADERS is non-nil, include headers with the -H option."
   (let* ((data (plist-get info :data))
          ;; We have to let-bind the following three since their dynamic
          ;; values are used for key lookup and url resolution
@@ -2655,22 +2657,20 @@ INFO contains the request data, UUID is a unique identifier."
          (url (let ((backend-url (gptel-backend-url gptel-backend)))
                 (if (functionp backend-url)
                     (funcall backend-url) backend-url)))
-         (data-json (decode-coding-string (gptel--json-encode data) 'utf-8 t))
-         (headers
-          (append '(("Content-Type" . "application/json"))
-                  (when-let* ((header (gptel-backend-header gptel-backend)))
-                    (if (functionp header)
-                        (funcall header) header)))))
-    (when gptel-log-level
-      (when (eq gptel-log-level 'debug)
-        (gptel--log (gptel--json-encode
-                     (mapcar (lambda (pair) (cons (intern (car pair)) (cdr pair)))
-                             headers))
-                    "request headers"))
-      (gptel--log data-json "request body"))
+         (data-json (decode-coding-string (gptel--json-encode data) 'utf-8 t)))
+    (when gptel-log-level (gptel--log data-json "request body"))
     (append
      gptel-curl--common-args
      gptel-curl-extra-args
+     (if include-headers
+         (cl-loop
+          for (key . val) in
+          (append '(("Content-Type" . "application/json"))
+                  (when-let* ((header (gptel-backend-header gptel-backend)))
+                    (if (functionp header)
+                        (funcall header) header)))
+          collect (format "-H%s: %s" key val))
+       (list "-H@-"))
      (and-let* ((curl-args (gptel-backend-curl-args gptel-backend)))
        (if (functionp curl-args) (funcall curl-args) curl-args))
      (list (format "-w(%s . %%{size_header})" uuid))
@@ -2688,8 +2688,6 @@ INFO contains the request data, UUID is a unique identifier."
        (list "--proxy" gptel-proxy
              "--proxy-negotiate"
              "--proxy-user" ":"))
-     (cl-loop for (key . val) in headers
-              collect (format "-H%s: %s" key val))
      (list url))))
 
 ;;;###autoload
@@ -2713,13 +2711,13 @@ the response is inserted into the current buffer after point."
                             (recent-keys))))
          (info (gptel-fsm-info fsm))
          (backend (plist-get info :backend))
-         (args (gptel-curl--get-args info uuid))
+         (args (gptel-curl--get-args info uuid nil))
          (stream (plist-get info :stream))
-         (process (apply #'start-process "gptel-curl"
-                         (gptel--temp-buffer " *gptel-curl*") (gptel--curl-path) args)))
-    (when (eq gptel-log-level 'debug)
-      (gptel--log (mapconcat #'shell-quote-argument (cons (gptel--curl-path) args) " \\\n")
-                  "request Curl command" 'no-json))
+         (process (make-process
+                   :name "gptel-curl"
+                   :buffer (gptel--temp-buffer " *gptel-curl*")
+                   :command (cons (gptel--curl-path) args)
+                   :connection-type 'pipe)))
     (with-current-buffer (process-buffer process)
       (cond
        ((eq (gptel-backend-coding-system backend) 'binary)
@@ -2732,6 +2730,24 @@ the response is inserted into the current buffer after point."
 	;; for cases when buffer coding system is not set to utf-8.
 	(set-process-coding-system process 'utf-8-unix 'utf-8-unix)))
       (set-process-query-on-exit-flag process nil)
+      (let* ((gptel-backend backend) ;Required for header function's environment
+             (gptel-model (plist-get info :model))
+             (headers
+              (append '(("Content-Type" . "application/json"))
+                      (when-let* ((header (gptel-backend-header backend)))
+                        (if (functionp header)
+                            (funcall header) header)))))
+        (when (eq gptel-log-level 'debug)
+          (gptel--log (gptel--json-encode
+                       (mapcar (lambda (pair) (cons (intern (car pair)) (cdr pair)))
+                               headers))
+                      "request headers")
+          (gptel--log (mapconcat #'shell-quote-argument
+                                 (cons (gptel--curl-path) args) " \\\n")
+                      "request Curl command" 'no-json))
+        (dolist (header headers)
+          (process-send-string process (concat (car header) ": " (cdr header) "\n"))))
+      (process-send-eof process)
       (if (plist-get info :uuid)        ;not the first run, set only the uuid
           (plist-put info :uuid uuid)
         (setf (gptel-fsm-info fsm)      ;fist run, set all process parameters

gptel.el
@@ -1662,7 +1662,7 @@ kill ring instead."
           (plist-put (gptel-fsm-info gptel--fsm-last) :data data)
           (if copy                 ;Copy Curl command instead of sending request
               (let ((args (gptel-curl--get-args (gptel-fsm-info gptel--fsm-last)
-                                                (md5 (format "%s" (random))))))
+                                                (md5 (format "%s" (random))) t)))
                 (kill-new
                  (mapconcat #'shell-quote-argument
                             (cons (gptel--curl-path) args) " \\\n"))

commit 3ad7a36250eaed8b68bb7fbd264877ef62bdf2b5

gptel-request: Add -N flag to Curl invocation on all platforms
* gptel-request.el (gptel-curl--common-args): The -N
flag (no-buffering) was previously added for macOS in
4747ffa251af84dbb6fe181f5d2b484ec2e442a7, but
85126d398c800dfed80a91aac990a5dc0d15f9a6 caused a regression on
Linux as well.

Add it to the Curl invocation on all platforms.  This is a
tentative change, to be reverted if there are issues.

gptel-request.el
@@ -727,13 +727,13 @@ See `gptel-backend'."
   (cond
    ((memq system-type '(windows-nt ms-dos))
     '("--disable" "--location" "--silent" "-XPOST"
-      "-y7200" "-Y1" "-D-"))
+      "-y7200" "-Y1" "-N" "-D-"))
    ((eq system-type 'darwin)
     '("--disable" "--location" "--silent" "--compressed"
       "-XPOST" "-y7200" "-Y1" "-N" "-D-"))
    (t
     '("--disable" "--location" "--silent" "--compressed"
-      "-XPOST" "-y7200" "-Y1" "-D-")))
+      "-XPOST" "-y7200" "-Y1" "-N" "-D-")))
   "Arguments always passed to Curl for gptel queries.")
 
 (defvar gptel--link-type-cache nil