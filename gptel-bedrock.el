b806759d5df001db082089f46d7a63aa06358443
Daniel Fleischer <danflscr@gmail.com>
2026-05-04 21:26:00 +0300

    Bedrock Opus 4.7
    - Added Claude Opus 4.7 model ID to Bedrock model list.
    - Cleaned white space in `gptel-bedrock.el`.
    - Added NEWS item.

--- gptel-bedrock.el
+++ gptel-bedrock.el
@@ -600,6 +600,7 @@ Convenient to use with `cl-multiple-value-bind'"
   ;; https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html
   '((claude-sonnet-4-6           . "anthropic.claude-sonnet-4-6")
     (claude-opus-4-6             . "anthropic.claude-opus-4-6-v1")
+    (claude-opus-4-7             . "anthropic.claude-opus-4-7")
     (claude-sonnet-4-5-20250929  . "anthropic.claude-sonnet-4-5-20250929-v1:0")
     (claude-haiku-4-5-20251001   . "anthropic.claude-haiku-4-5-20251001-v1:0")
 	(claude-opus-4-5-20251101    . "anthropic.claude-opus-4-5-20251101-v1:0")
