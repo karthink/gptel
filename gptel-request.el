ae35aadde293b63a90dcc9891ace2f183922c489
Pedro Bruel <bruel@hpe.com>
2026-06-01 08:42:29 -0700

    Add reasoning chunks and blocks to dolist

--- gptel-request.el
+++ gptel-request.el
@@ -1894,7 +1894,7 @@ MACHINE is an instance of `gptel-fsm'"
   ;; a second network request: gptel tests for the presence of these flags to
   ;; handle state transitions.  (NOTE: Don't add :uuid to this.)
   (let ((req-info (gptel-fsm-info fsm)))
-    (dolist (key '(:tool-result :tool-use :error :http-status :reasoning :tokens))
+    (dolist (key '(:tool-result :tool-use :error :http-status :reasoning :reasoning-block :reasoning-chunks :reasoning-marker :tokens))
       (when (plist-get req-info key)
         (plist-put req-info key nil)))
     (funcall
