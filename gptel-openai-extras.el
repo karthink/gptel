9cafa41e7172a995e4eeadb5049a8c05bf7c30ba
陈虎明 <chenhuming@gmail.com>
2026-07-16 18:06:53 +0800

    Remove soon to be deprecated deepseek-chat and deepseek-reasoner model.

--- gptel-openai-extras.el
+++ gptel-openai-extras.el
@@ -332,17 +332,7 @@ This method works by wrapping the main implementation, passing PROMPTS."
           (host "api.deepseek.com")
           (protocol "https")
           (endpoint "/v1/chat/completions")
-          (models '((deepseek-reasoner
-                     :capabilities (tool reasoning)
-                     :context-window 128
-                     :input-cost 0.14
-                     :output-cost 0.28)
-                    (deepseek-chat
-                     :capabilities (tool)
-                     :context-window 128
-                     :input-cost 0.14
-                     :output-cost 0.28)
-		    (deepseek-v4-flash
+          (models '((deepseek-v4-flash
                      :capabilities (tool reasoning)
                      :context-window 1000
                      :input-cost 0.14
